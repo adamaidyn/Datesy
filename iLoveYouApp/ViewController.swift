a015fdcb29448d30bab8bd03f3eac99ec8d3b400
Adil Aidyn <adilbeisenov15@gmail.com>
2021-10-18 11:49:46 +0600

    Initial Commit

---
+++ iLoveYouApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iLoveYouApp
+//
+//  Created by Adm Aidyn on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemIndigo
+    }
+
+
+}
+
